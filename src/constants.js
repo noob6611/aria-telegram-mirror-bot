f8781e98e276d7999074632fcb4503af535bfcee
noob6611 <karan.mazerunner@gmail.com>
2020-07-09 01:15:42 +0530

    improved

---
+++ src/constants.js
@@ -0,0 +1,28 @@
+module.exports = Object.freeze({
+  TOKEN: '1350999892:AAEyTyl4KguLW5HavPrg_WtACU40k8PAtcU',
+  ARIA_SECRET: 'abc123abc123',
+  ARIA_DOWNLOAD_LOCATION: 'path/to/aria-telegram-mirror-bot/downloads (no trailing "/")',
+  ARIA_DOWNLOAD_LOCATION_ROOT: '/drive', //The mountpoint that contains ARIA_DOWNLOAD_LOCATION
+  ARIA_FILTERED_DOMAINS: ['yts', 'YTS', 'cruzing.xyz', 'eztv.ag', 'YIFY'], // Prevent downloading from URLs containing these substrings
+  ARIA_FILTERED_FILENAMES: ['YIFY'], // Files/top level directories with these substrings in the filename won't be downloaded
+  ARIA_PORT: 8210, // Port for aria2c RPC server, if you change this here, make sure to update aria.sh as well
+  GDRIVE_PARENT_DIR_ID:"1g0aeYatWorlLGGnu8H5CzFZhB-jEu-dP" 'id_of_Drive_folder_to_upload_into',
+  SUDO_USERS: [1181331306],	// Telegram user IDs. These users can use the bot in any chat.
+  AUTHORIZED_CHATS: [-1001477458762],	// Telegram chat IDs. Anyone in these chats can use the bot.
+  STATUS_UPDATE_INTERVAL_MS: 12000, // A smaller number will update faster, but might cause rate limiting
+  DRIVE_FILE_PRIVATE: {
+    ENABLED: false,
+    EMAILS: ['someMail@gmail.com', 'someOtherMail@gmail.com']
+  },
+  DOWNLOAD_NOTIFY_TARGET: {  // Information about the web service to notify on download completion.
+    enabled: false,   // Set this to true to use the notify functionality
+    host: 'hostname.domain',
+    port: 80,
+    path: '/botNotify'
+  },
+  COMMANDS_USE_BOT_NAME: {
+    ENABLED: true,  // If true, all commands except '/list' has to have the bot username after the command
+    NAME: "@reaper_urluploadbot"
+  },
+  IS_TEAM_DRIVE: true
+});
